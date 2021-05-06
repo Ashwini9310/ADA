ff420d19eae4945df030013e78e75acc21e7d474
Ashwini9310 <83028084+Ashwini9310@users.noreply.github.com>
2021-05-06 16:56:18 +0530

    Create DFS.c

---
+++ BFS_DFS/DFS.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<time.h>
+ 
+void DFS(int);
+int G[10][10],visited[10],n;    
+ 
+void main()
+{
+    int i,j;
+     clock_t start, end;
+     
+    printf("Enter number of vertices:");
+   
+	scanf("%d",&n);
+ 
+    //read the adjecency matrix
+	printf("\nEnter adjecency matrix of the graph:");
+   
+	for(i=0;i<n;i++)
+       for(j=0;j<n;j++)
+			scanf("%d",&G[i][j]);
+			
+	start = clock();
+	DFS(i);
+    end = clock();
+    float ti = ((double)(end - start)/CLOCKS_PER_SEC);
+ 
+    //visited is initialized to zero
+   for(i=0;i<n;i++)
+        visited[i]=0;
+ 
+    DFS(0);
+    printf("\nTime taken: %f", ti);
+}
+ 
+void DFS(int i)
+{
+    int j;
+	printf("\n%d",i);
+    visited[i]=1;
+	
+	for(j=0;j<n;j++)
+       if(!visited[j]&&G[i][j]==1)
+            DFS(j);
+}
